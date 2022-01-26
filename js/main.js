350d86e7acbae5032714dd15b50665e648552389
shaykatWorks <shaykatworks@gmail.com>
2022-01-27 00:58:28 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+  $(".search_icon").click(function(){
+    $(".search-container").toggle();
+  });
+});
+//===================--search bar end--===================
+  $(document).ready(function(){
+    $(".menubar").click(function(){
+      $(".mainmenu").toggle();
+    });
+  });
+
+//===================--MANU bar end--===================
+$(document).ready(function(){
+    $('.image_inner').slick({
+        infinite: true,
+        cssEase: 'linear',
+        // autoplay: 600,
+        dots:true,
+        arrows:true,
+        // fade: true,
+        cssEase: 'linear',
+    
+      })
+});
+//===================--SLIKE slider end--===================
+
+    
+
